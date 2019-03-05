d9e362a5e6ec14960893615c2acd98082d0fd09a
Richard ONeil <richard.oneil@garvin-allen.com>
2019-03-05 15:28:26 -0400

    Initial commit

---
+++ Backender/BackFoo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Backender
+{
+    public class BackFoo
+    {
+        public void That()
+        {
+            Console.WriteLine("BackFoo says dat!");
+        }
+    }
+}
---
+++ DynamicRunner/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace DynamicRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoSomethingSoft();
+            Console.ReadKey();
+        }
+        
+        private static void DoSomethingSoft()
+        {
+            var myClass = new MyClass();
+            // from file
+            string lines = File.ReadAllText(@"C:\source\prototypes\DynamicRunner\DynamicRunner\softclass.txt", Encoding.UTF8);
+            // from string
+            //string line = "public class FooClass { public string Execute() { return \"output!\";}}";
+            
+            using (Microsoft.CSharp.CSharpCodeProvider foo = 
+                new Microsoft.CSharp.CSharpCodeProvider())
+            {
+                var res = foo.CompileAssemblyFromSource(
+                    new System.CodeDom.Compiler.CompilerParameters() 
+                    {  
+                        GenerateExecutable = false,
+                        GenerateInMemory = true,
+                        ReferencedAssemblies =
+                        {
+                            Assembly.GetExecutingAssembly().Location,
+                            "Backender.dll"
+                        }
+                    },
+                    lines
+                );
+
+                var type = res.CompiledAssembly.GetType("FooClass");
+
+                var obj = Activator.CreateInstance(type);
+
+                var output = type.GetMethod("Execute").Invoke(obj, new object[] { "richard", myClass });
+                
+                Console.WriteLine(output);
+            }
+        }
+    }
+
+    public class MyClass
+    {
+        public void Execute()
+        {
+            Console.WriteLine("This is from the MyClass external class");
+        }
+    }
+    
+    public class YourClass
+    {
+        public void Execute()
+        {
+            Console.WriteLine("This is from the YourClass external class");
+        }
+    }
+}
